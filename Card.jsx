efa30c0ffa7d9bb53e0a95a8608584690a5312b6
talangi12 <talangicharles@gmail.com>
2025-06-27 18:21:44 +0300

    Create Card.jsx

---
+++ Card.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Card = ({ children, className = '' }) => {
+  return (
+    <div className={`bg-white dark:bg-gray-800 shadow-md rounded-lg p-6 ${className}`}>
+      {children}
+    </div>
+  );
+};
+
+Card.propTypes = {
+  children: PropTypes.node.isRequired,
+  className: PropTypes.string,
+};
+
+export default Card;
