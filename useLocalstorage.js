38d5ed9796272eb5f98e42994f1f7dccf3956d2c
talangi12 <talangicharles@gmail.com>
2025-06-27 19:28:32 +0300

    Create useLocalstorage.js

---
+++ useLocalstorage.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from 'react';
+
+function useLocalStorage(key, initialValue) {
+  // Get initial value from local storage or use the provided initialValue
+  const [storedValue, setStoredValue] = useState(() => {
+    try {
+      const item = window.localStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      console.error("Error reading from localStorage:", error);
+      return initialValue;
+    }
+  });
+
+  // useEffect to update local storage when the state changes
+  useEffect(() => {
+    try {
+      window.localStorage.setItem(key, JSON.stringify(storedValue));
+    } catch (error) {
+      console.error("Error writing to localStorage:", error);
+    }
+  }, [key, storedValue]);
+
+  return [storedValue, setStoredValue];
+}
+
+export default useLocalStorage;
